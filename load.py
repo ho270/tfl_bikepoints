42c8ce0ce812ff33a4648f24e8cecaa2943cc6ab
TIL-harrietowen <harriet.owen@theinformationlab.com>
2026-05-05 14:29:50 +0100

    upload to aws bucket script created

--- load.py
+++ load.py
@@ -3,9 +3,28 @@
 from dotenv import load_dotenv
 import os
 import boto3
+import logging
+from datetime import datetime
+from pathlib import Path
+
+#logging configuration
+log_dir = 'logs'
+os.makedirs(log_dir,exist_ok=True)
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+log_filename = f'{log_dir}/load_{timestamp}.log'
+
+logging.basicConfig(
+    filename=log_filename,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+log = logging.getLogger()
+log.info('Logger intialised')
 
 #load in environment variables
 load_dotenv()
+log.info("Environment variables loaded")
 
 #variables
 aws_key = os.getenv('AWS_KEY_ID')
@@ -19,8 +38,23 @@ s3_client = boto3.client(
     aws_secret_access_key = aws_secret
 )
 
-#save data into S3 bucket
-data = 'data/2026-05-05_09-32-32.json'
-filename = '2026-05-05_09-32-32.json'
+#create data directory
+data_dir = Path('data')
+files = list(data_dir.glob(f'*.json'))
 
-s3_client.upload_file(data, bucket, filename)
+#loop through the data directory and upload to S3
+processed = 0
+for file in files:
+    filename = os.path.basename(file)
+
+    try:
+        s3_client.upload_file(file,bucket,filename)
+        log.info(f'{file} uploaded to S3')
+        s3_client.head_object(Bucket=bucket,Key=filename)
+        os.remove(file)
+        log.info(f'{file} deleted locally from {data_dir}')
+        processed +=1
+    except Exception as e:
+            log.error(e)
+
+log.info(f'Finished uploading {processed} files')
